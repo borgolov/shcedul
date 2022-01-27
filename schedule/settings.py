0297aa7bee5b0bd999b968325d0d3e58458f3618
borgolov <borgolov.alex@gmail.com>
2022-01-27 14:31:01 +0800

    fix 2022/01

--- schedule/settings.py
+++ schedule/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'schedule.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'base/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
